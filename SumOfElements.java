cba1b3286c2225c51474a4a22c409f59b2feee78
Soumya Shimpi <145319455+Soumya-Shimpi@users.noreply.github.com>
2026-06-19 11:30:26 +0530

    Add files via upload

---
+++ SumOfElements.java
@@ -0,0 +1,14 @@
+class SumOfElements
+{
+    public static void main(String[] args)
+    {
+        int [] num ={4,1,4,3,2};
+        int sum=0;
+        for(int i=0;i<num.length; i++)
+        {
+            sum =sum + num[i];
+        }
+        System.out.println(sum);
+    }
+    
+}
